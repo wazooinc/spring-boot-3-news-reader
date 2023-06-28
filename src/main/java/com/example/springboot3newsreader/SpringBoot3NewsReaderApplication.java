52e809d2402f01349ee09724f28bf1e59518ad66
Erik Yuzwa <erikyuzwa@gmail.com>
2023-06-28 12:44:19 -0600

    initial commit

---
+++ src/main/java/com/example/springboot3newsreader/SpringBoot3NewsReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3newsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3NewsReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3NewsReaderApplication.class, args);
+	}
+
+}
